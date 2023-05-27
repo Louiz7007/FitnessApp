9cfb7e8a0c085f1eb1f376cdeb55c8193ff82a6a
Louiz7007 <louis.dittmann@web.de>
2023-05-27 14:34:27 +0200

    Added Profile Activity and Designed Screen

---
+++ app/src/main/java/com/example/fitnessapp/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.fitnessapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fitnessapp.databinding.ActivityProfileBinding;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ActivityProfileBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+    }
+
+}
