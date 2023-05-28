2a90546fc63e079ae85e8bd731ba5564ffba61f1
Louiz7007 <louis.dittmann@web.de>
2023-05-28 11:48:42 +0200

    Created DBOpenHelper with 2 Tables "user" and "trainings". Added some Selects and Inserts + check if user exists.

---
+++ app/src/main/java/com/example/fitnessapp/DBOpenHelper.java
@@ -0,0 +1,95 @@
+package com.example.fitnessapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    private static String DATABASE_NAME = "FitnessDB";
+    private static int DATABASE_VERSION = 1;
+
+    private final String SELECT_BY_NAME = "SELECT * FROM user WHERE firstname = %s AND lastname = %s";
+    private final String SELECT_TRAINING_BY_NAME_AND_INTENSITY = "SELECT * FROM training WHERE trainingname = %s AND intensity = %s";
+    private final String CREATE_TABLE_USER = "CREATE TABLE user (_id INTEGER PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), age INTEGER, weight DECIMAL(5,2), workoutlevel INTEGER CHECK(workoutlevel >= 0 AND workoutlevel < 4))";
+    private final String CREATE_TABLE_TRAININGS = "CREATE TABLE trainings (_id INTEGER PRIMARY KEY, trainingName VARCHAR(255), intensity VARCHAR(255), metValue DECIMAL(3, 1))";
+
+    public DBOpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_USER);
+        db.execSQL(CREATE_TABLE_TRAININGS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(DBOpenHelper.class.getSimpleName(), "Upgrades werden nicht unterstützt.");
+    }
+
+    // Insert a Row into the user Table
+    public void insertUser(String firstname, String lastname, int age, double weight, int workoutlevel){
+        ContentValues values = new ContentValues();
+        values.put("firstname", firstname);
+        values.put("lastname", lastname);
+        values.put("age", age);
+        values.put("weight", weight);
+        values.put("workoutlevel", workoutlevel);
+        long rowId = getWritableDatabase().insert("user", null, values);
+        Log.d(DBOpenHelper.class.getSimpleName(), "Insert into user_Table: " + rowId);
+    }
+
+    // Insert a Row into the Trainings Table
+    public void insertTraining(String trainingName, String intensity, double metValue) {
+        ContentValues values = new ContentValues();
+        values.put("trainingName", trainingName);
+        values.put("intensity", intensity);
+        values.put("metValue", metValue);
+        long rowId = getWritableDatabase().insert("trainings", null, values);
+        Log.d(DBOpenHelper.class.getSimpleName(), "Insert into Trainings_Table: " + rowId);
+    }
+
+    // Returns Cursor with all from trainings Table
+    public Cursor selectAllFromTrainings() {
+        return getReadableDatabase().rawQuery("SELECT * FROM trainings", null);
+    }
+
+    // Select Training by Name and Intensity
+    public Cursor selectTrainingByNameAndIntensity(String trainingName, String intensity) {
+        return getReadableDatabase().rawQuery(String.format(SELECT_TRAINING_BY_NAME_AND_INTENSITY, trainingName, intensity), null);
+    }
+
+    // Returns Cursor with all from user Table
+    public Cursor selectAllFromUser(){
+        return getReadableDatabase().rawQuery("SELECT * FROM user", null);
+    }
+
+    // Returns Cursor of User Row called by first and lastname
+    public Cursor selectUserByName(String firstname, String lastname) {
+        return getReadableDatabase().rawQuery(String.format(SELECT_BY_NAME, firstname, lastname), null);
+    }
+
+    public boolean userExists() {
+        boolean result;
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM user", null);
+        if(cursor.moveToNext()){
+            result = true;
+        }else {
+            result = false;
+        }
+        cursor.close();
+        return result;
+    }
+
+    // Drop whole user Table
+    public void dropTableUser() {
+        getWritableDatabase().rawQuery("DROP TABLE IF EXISTS user", null);
+    }
+}
