79dbf437c576a28c34b6620191f3d30b70592c6e
Roman <adrianromanik@googlemail.com>
2023-06-01 00:44:48 +0200

    Api Works fine..

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.fitnessapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.fitnessapp.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_LOCATION = 12345;
     private ActivityMainBinding binding;
+    private LocationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +37,41 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavController navController = Navigation.findNavController(this,
+                                                                   R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+    }
+
+    public Location getCurrentLocation() {
+
+
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                                                      Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                                      Manifest.permission.ACCESS_COARSE_LOCATION}
+                    , REQUEST_LOCATION);
+
+        }
+        try {
+
+            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
     }
     @Override
     public boolean onCreateOptionsMenu (Menu menu) {
--- app/src/main/java/com/example/fitnessapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/fitnessapp/ui/home/HomeFragment.java
@@ -1,20 +1,40 @@
 package com.example.fitnessapp.ui.home;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.fitnessapp.databinding.FragmentHomeBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HomeFragment extends Fragment {
 
+    private static final int REQUEST_LOCATION = 12345;
+    LocationManager manager;
     private FragmentHomeBinding binding;
+    private Location currentLocation;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,15 +43,82 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        manager = getActivity().getSystemService(LocationManager.class);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        getCurrentLocation();
+
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+
+    public void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                                               Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(),
+                                                      Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                                              new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                                      Manifest.permission.ACCESS_COARSE_LOCATION}
+                    , REQUEST_LOCATION);
+
+        }
+
+        LocationListener listener = (location -> {
+
+            currentLocation = location;
+            getWeatherInfos(location);
+
+        });
+
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, listener);
+
+    }
+
+    private void getWeatherInfos(Location location) {
+
+        String apiKey = "ebf300fa0bce8fb61fe75315e514ade9";
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(
+                    String.format("https://api.openweathermap.org/data/2" +
+                                          ".5/weather?lat=%s&lon=%s&appid=%s",
+                                  location.getLatitude(), location.getLongitude(),
+                                  apiKey)
+            ).openConnection();
+            System.setProperty("http.keepAlive", "false");
+            if (con.getResponseCode() != HttpURLConnection.HTTP_OK)
+                return;
+            StringBuilder builder = new StringBuilder();
+            new BufferedReader(new InputStreamReader(con.getInputStream())).lines().forEach(builder::append);
+            con.disconnect();
+
+            JSONObject json = new JSONObject(builder.toString());
+
+            getActivity().runOnUiThread(() -> {
+
+                binding.textHome.setText(json.toString());
+
+            });
+
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
+
