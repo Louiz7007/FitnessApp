9f44fd10be31623f34527d8f245e87d86e710ada
adriahnogementa <adrianromanik@googlemail.com>
2023-06-02 19:06:50 +0200

    New Fragments for new Training

--- app/src/main/java/com/example/fitnessapp/ui/dashboard/NewTrainingPlanFragment.java
+++ app/src/main/java/com/example/fitnessapp/ui/dashboard/NewTrainingPlanFragment.java
@@ -2,7 +2,10 @@ package com.example.fitnessapp.ui.dashboard;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavAction;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,57 +13,30 @@ import android.view.ViewGroup;
 
 import com.example.fitnessapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewTrainingPlanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NewTrainingPlanFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public NewTrainingPlanFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewTrainingPlanFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewTrainingPlanFragment newInstance(String param1, String param2) {
-        NewTrainingPlanFragment fragment = new NewTrainingPlanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        OnBackPressedCallback callback = new OnBackPressedCallback(true ) {
+            @Override
+            public void handleOnBackPressed() {
+                Navigation.findNavController(requireView()).navigate(R.id.action_newTrainingPlanFragment_to_navigation_dashboard);
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
         return inflater.inflate(R.layout.fragment_new_training_plan, container, false);
     }
 }
