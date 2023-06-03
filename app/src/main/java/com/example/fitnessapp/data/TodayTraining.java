1143df9c0e4603541083ef27c44a8835a717946d
Louiz7007 <louis.dittmann@web.de>
2023-06-03 13:54:05 +0200

    Anzeige der Datenbank Heutige Trainings

--- app/src/main/java/com/example/fitnessapp/data/TodayTraining.java
+++ app/src/main/java/com/example/fitnessapp/data/TodayTraining.java
@@ -2,20 +2,28 @@ package com.example.fitnessapp.data;
 
 public class TodayTraining {
 
+    private int id;
     private String name;
     private String intensity;
     private double metValue;
     private boolean success;
 
-    public TodayTraining(String name, String type, double speed, boolean success) {
+    public TodayTraining(int id, String name, String type, double speed, boolean success) {
+        this.id = id;
         this.name = name;
         this.intensity = type;
         this.metValue = speed;
         this.success = success;
     }
 
+    public int getId() {return id;}
+
     public boolean getSuccess() {
         return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
     }
 
     public String getName() {
