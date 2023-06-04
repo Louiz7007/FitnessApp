cc93f5e76433fee3e8aa66abcb70914f5979ab46
adriahnogementa <adrianromanik@googlemail.com>
2023-06-04 18:12:39 +0200

    Trainings aus TrainingsPlan können hinzugefügt werden.

--- app/src/main/java/com/example/fitnessapp/DBOpenHelper.java
+++ app/src/main/java/com/example/fitnessapp/DBOpenHelper.java
@@ -14,6 +14,7 @@ import com.example.fitnessapp.data.Training;
 import com.example.fitnessapp.data.TrainingsPlan;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -22,6 +23,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "FitnessDB";
     private static final int DATABASE_VERSION = 1;
+
 //    private final String SELECT_TRAINING_BY_NAME_AND_INTENSITY = "SELECT * FROM training WHERE " +
 //            "trainingname = %s AND intensity = %s";
 //    private final String SELECT_USERTRAINING_BY_TRAINING_AND_DATE = "SELECT * FROM usertrainings " +
@@ -47,6 +49,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
     private final String SELECT_TRAININGSPLAN_BY_NAME = "SELECT * FROM trainingsplan WHERE name = \"%s\"";
     private final String SELECT_TRAINING_BY_NAME_AND_INTENSITY = "SELECT * FROM training WHERE trainingname = %s AND intensity = %s";
     private final String SELECT_USERTRAINING_BY_TRAINING_AND_DATE = "SELECT * FROM usertrainings WHERE date = %s AND idTraining = %s";
+    private final String SELECT_USERTRAINING_FROM_NOW = "SELECT usertrainings._id, trainingName, intensity, metValue, usertrainings.datetime, usertrainings.duration FROM trainings INNER JOIN usertrainings ON trainings._id = usertrainings.idTraining WHERE usertrainings.datetime >= datetime('now') ORDER BY usertrainings.datetime ASC";
     private final String SELECT_USERTRAINING_BY_DATE = "SELECT usertrainings._id, trainingName, intensity, duration, metValue, success FROM trainings INNER JOIN usertrainings ON trainings._id = usertrainings.idTraining WHERE usertrainings.datetime BETWEEN \"%s 00:00:00\" AND \"%s 23:59:59\"";
     private final String CREATE_TABLE_USER = "CREATE TABLE user (_id INTEGER PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), age INTEGER, weight DECIMAL(5,2), workoutlevel INTEGER CHECK(workoutlevel >= 0 AND workoutlevel < 4))";
     private final String CREATE_TABLE_TRAININGS = "CREATE TABLE trainings (_id INTEGER PRIMARY KEY, trainingName VARCHAR(255), intensity VARCHAR(255), metValue DECIMAL(3, 1))";
@@ -194,6 +197,20 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         return sum;
     }
 
+    public ArrayList<Training> selectAllUserTrainingsFromNow(){
+        Cursor cursor = getReadableDatabase().rawQuery(SELECT_USERTRAINING_FROM_NOW, null);
+
+        ArrayList<Training> trainingList = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            Training training = new Training(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getDouble(3));
+            training.setDate(Timestamp.valueOf(cursor.getString(4)));
+            training.setDuration((int) cursor.getDouble(5));
+            trainingList.add(training);
+        }
+    return trainingList;
+    }
+
     public void insertUserTrainingWithTrainingObject(Training training) {
         ContentValues values = new ContentValues();
         values.put("idTraining", training.getId());
@@ -242,6 +259,10 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         getWritableDatabase().delete("usertrainings", null, null);
     }
 
+    public void deleteUsertraingsByTrainingId(int id) {
+        getWritableDatabase().delete("usertrainings", "idTraining=?", new String[]{String.valueOf(id)});
+    }
+
     // Returns Cursor with all from trainings Table
     public Cursor selectAllFromTrainings() {
         return getReadableDatabase().rawQuery("SELECT * FROM trainings", null);
@@ -282,4 +303,18 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         return result;
     }
 
+    public void updateUsertraining(Training training) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("datetime", training.getTimestamp().toString());
+        values.put("duration", training.getDuration());
+
+        String whereClause = "_id = ?";
+        String[] whereArgs = {String.valueOf(training.getId())};
+
+        db.update("usertrainings", values, whereClause, whereArgs);
+
+
+    }
 }
